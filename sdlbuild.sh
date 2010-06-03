8ee3bf74e5bbf22b79d70a30fcbda1529093f38f
Scott Anderson <smanders@bluepill>
2010-06-03 11:39:03 -0600

    Fixed sdlbuild.sh

--- sdlbuild.sh
+++ sdlbuild.sh
@@ -1,71 +1,6 @@
 #!/bin/bash
 
 cd $(dirname $0)
-curDir=$(pwd)
-
-function usage()
-{
-  echo -e "Usage: $(basename $0) [-cl] [--copy] [--lock] <path to sdlExtern>"                     >&2
-  echo -e "\tWhere:\t[ -c | --copy ]     - tells it to copy the binaries to sdlExtern"            >&2
-  echo -e "\t\t[ -l | --lock ]     - tells it to first svn lock the binaries in sdlExtern"        >&2
-  echo -e "\t\t<path to sdlExtern> - is the relative or absolute path to the sdlExtern directory" >&2
-  exit 1
-}
-
-let copy=0
-let lock=0
-
-# Parse the options and parameters.
-cmdLine="$*"
-while getopts "cl-:?h" Option
-do
-#{
-  case ${Option} in
-  #{
-    # This will copy the binaries to the sdlExtern directory
-    c ) let copy=1 ;;
-
-    # This will svn lock the binaries in the sdlExtern directory before copying
-    l ) let lock=1 ;;
-
-    - ) if [ "${OPTARG}" == "copy" ]
-        then
-          # This will copy the binaries to the sdlExtern directory
-          let copy=1
-        elif [ "${OPTARG}" == "lock" ]
-        then
-          # This will svn lock the binaries in the sdlExtern directory before copying
-          let lock=1
-        elif [ "${OPTARG}" == "help" ]
-        then
-          # Display the usage of this tool and quit.
-          usage
-        fi ;;
-
-    # Display usage and quit.
-    '?' ) usage ;;
-    'h' ) usage ;;
-  #}
-  esac
-#}
-done
-shift $(($OPTIND - 1))
-
-# Parse the parameter.
-if [ -z "${1}" ]
-then
-  usage
-else
-  SDLEXTERN=${1}
-  if [[ ! -d ${SDLEXTERN}/include ]]; then
-    echo "Error: $1 does not appear to be sdlExtern" >&2
-    usage
-  fi
-fi
-# Make sure that it is an absolute path
-cd ${SDLEXTERN}
-SDLEXTERN=`pwd`
-cd - >/dev/null
 
 OS=$(uname -s)
 MAKECMD=gmake
@@ -74,6 +9,7 @@ MAKECMD=make
 fi
 DIRDBG=_${OS}_Debug
 DIRREL=_${OS}_Release
+DIRXTN=_${OS}_Extern
 
 # Clean build -- if the directories exist, we'll wipe 'em out here
 if [[ -d ${DIRDBG} ]]; then
@@ -82,23 +18,41 @@ fi
 if [[ -d ${DIRREL} ]]; then
   rm -Rf ${DIRREL}
 fi
+if [[ -d ${DIRXTN} ]]; then
+  rm -RF ${DIRXTN}
+fi
 
 mkdir ${DIRDBG}
 cd ${DIRDBG}
 # Debug
-CXXFLAGS="-O2 -mcpu=ultrasparc -Wno-unknown-pragmas" CFLAGS="-O2 -mcpu=ultrasparc -Wno-unknown-pragmas" CXXFLAGS="-mcpu=ultrasparc" CFLAGS="-mcpu=ultrasparc" ../configure --with-gtk=2 --with-opengl --with-libjpeg=builtin --with-libpng=builtin --with-libtiff=builtin --with-expat=builtin --with-regex=builtin --with-zlib=builtin --disable-shared --disable-precomp-headers --enable-display --enable-std_string --enable-std_iostreams --enable-debug --enable-debug_flag --enable-debug_info --enable-debug_gdb
+../configure --with-gtk=2 --with-opengl --with-libjpeg=builtin --with-libpng=builtin --with-libtiff=builtin --with-expat=builtin --with-regex=builtin --with-zlib=builtin --disable-shared --disable-precomp-headers --enable-display --enable-std_string --enable-std_iostreams --enable-debug --enable-debug_flag --enable-debug_info --enable-debug_gdb
 ${MAKECMD}
 cd ..
 
 mkdir ${DIRREL}
 cd ${DIRREL}
 # Release
-CXXFLAGS="-O2 -mcpu=ultrasparc -Wno-unknown-pragmas" CFLAGS="-O2 -mcpu=ultrasparc -Wno-unknown-pragmas" CXXFLAGS="-mcpu=ultrasparc" CFLAGS="-mcpu=ultrasparc" ../configure --with-gtk=2 --with-opengl --with-libjpeg=builtin --with-libpng=builtin --with-libtiff=builtin --with-expat=builtin --with-regex=builtin --with-zlib=builtin --disable-shared --disable-precomp-headers --enable-display --enable-std_string --enable-std_iostreams
+../configure --with-gtk=2 --with-opengl --with-libjpeg=builtin --with-libpng=builtin --with-libtiff=builtin --with-expat=builtin --with-regex=builtin --with-zlib=builtin --disable-shared --disable-precomp-headers --enable-display --enable-std_string --enable-std_iostreams
 ${MAKECMD}
 cd ..
+./install-sh -c ${DIRREL}/lib/wx/config/gtk2-ansi-release-static-2.8 ${DIRREL}/thewxconfig
 
-# Now install the binaries.
-${curDir}/sdlinstall.sh ${cmdLine}
+# Create _*_Extern directory structure
+mkdir -p ${DIRXTN}/bin
+mkdir -p ${DIRXTN}/lib/wx/config
+mkdir -p ${DIRXTN}/lib/wx/include/gtk2-ansi-debug-static-2.8/wx
+mkdir -p ${DIRXTN}/lib/wx/include/gtk2-ansi-release-static-2.8/wx
+
+# Move/Copy to _*_Extern directory
+mv ${DIRDBG}/lib/libwx*.a ${DIRXTN}/lib
+mv ${DIRREL}/lib/libwx*.a ${DIRXTN}/lib
+cp ${DIRDBG}/lib/wx/config/gtk2-ansi-debug-static-2.8 ${DIRXTN}/lib/wx/config
+cp ${DIRDBG}/lib/wx/include/gtk2-ansi-debug-static-2.8/wx/setup.h ${DIRXTN}/lib/wx/include/gtk2-ansi-debug-static-2.8/wx
+cp ${DIRREL}/lib/wx/config/gtk2-ansi-release-static-2.8 ${DIRXTN}/lib/wx/config
+cp ${DIRREL}/lib/wx/include/gtk2-ansi-release-static-2.8/wx/setup.h ${DIRXTN}/lib/wx/include/gtk2-ansi-release-static-2.8/wx
+cp ${DIRREL}/utils/wxrc/wxrc ${DIRXTN}/bin
+cp ${DIRREL}/utils/wxrc/wxrc ${DIRXTN}/bin/wxrc-2.8
+cp ${DIRREL}/thewxconfig ${DIRXTN}/bin/wx-config
 
 echo "Done."
 
