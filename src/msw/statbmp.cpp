4155b67f33ae8d60ea1e3a46f9937b476dcc2041
Scott M Anderson <smanders@sdl.usu.edu>
2014-02-20 15:14:12 -0700

    reduce flickering in static ctrls

--- src/msw/statbmp.cpp
+++ src/msw/statbmp.cpp
@@ -136,6 +136,9 @@ bool wxStaticBitmap::Create(wxWindow *parent,
     }
 #endif // !__WXWINCE__
 
+    // reduce flickering
+    Bind(wxEVT_ERASE_BACKGROUND, [](wxEraseEvent&){});
+
     return true;
 }
 
